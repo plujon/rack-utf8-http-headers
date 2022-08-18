2f65fd39406f459c3a5f5473859e0ec98cd0bc4c
Jon Wilkes <jon@pluckeye.net>
2022-08-18 13:35:49 -0700

    HTTP_PLUCKEYE_B64_HN -> HTTP_PLUCKEYE_HN

--- lib/rack/Utf8HttpHeaders.rb
+++ lib/rack/Utf8HttpHeaders.rb
@@ -12,6 +12,14 @@ module Rack
           v.force_encoding 'UTF-8'
           env.delete(k) if !v.valid_encoding?
         end
+        if k.start_with?('HTTP_PLUCKEYE_B64_')
+          v = Base64.urlsafe_decode64(v)
+          v.force_encoding 'UTF-8'
+          if v.valid_encoding?
+            env[k.sub(/B64_/, '')] = v
+            env.delete(k)
+          end
+        end
       end
       @app.call(env)
     end
